55addc8343395d7d6193219f429f21b912050867
motosasha <39082380+motosasha@users.noreply.github.com>
2023-11-24 13:28:41 +0300

    Add files via upload

--- js/bundle.js
+++ js/bundle.js
@@ -1,2 +1,2 @@
-!function(){var r={643:function(){}},t={};!function n(o){var e=t[o];if(void 0!==e)return e.exports;var i=t[o]={exports:{}};return r[o](i,i.exports,n),i.exports}(643)}();
+!function(){var t={986:function(){window.addEventListener("load",(()=>{setTimeout((()=>document.body.dataset.state="loaded"),500)}))},643:function(){}},e={};function o(n){var r=e[n];if(void 0!==r)return r.exports;var d=e[n]={exports:{}};return t[n](d,d.exports,o),d.exports}o(986),o(643)}();
 //# sourceMappingURL=bundle.js.map
